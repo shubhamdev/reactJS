734bb8a1ccaf31fa96a8ba3cef422b096d7a6199
shubhamdev <shubham1838@gmail.com>
2017-12-16 01:47:53 +0530

    basic introduction of reactjs.

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+var config = {
+   entry: './app/main.js',
+   output: {
+      path:'/',
+      filename: 'index.js',
+   },
+   watch: true,
+   devServer: {
+      inline: true,
+      port: 3002
+   },
+   module: {
+      loaders: [
+         {
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+            query: {
+               presets: ['es2015', 'react']
+            }
+         }
+      ]
+   }
+}
+module.exports = config;
